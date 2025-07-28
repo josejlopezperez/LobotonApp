56b0e337bf6d56e8c8bec447b32864a8a0ef9ad8
Pepechuy <jose.lopez-perez@volvo.com>
2025-07-28 14:46:26 -0400

    [FEAT] Enhance game management with updated navigation and winner tracking in LobotonSheetWepApp

--- LobotonSheetWepApp.py
+++ LobotonSheetWepApp.py
@@ -6,8 +6,9 @@ import csv
 from Src.CourtInfo import CourtInfo
 
 if 'page' not in st.session_state: st.session_state.page = 0
-def GoWindows2(): st.session_state.page = 1
 def firstPage(): st.session_state.page = 0
+def secondPage(): st.session_state.page = 1
+def thirdPage(): st.session_state.page = 2
 
 class LobotonSheetWepApp():
     def __init__(self):
@@ -18,72 +19,88 @@ class LobotonSheetWepApp():
                 spamReader  = csv.reader(csvFile, delimiter=',', quotechar='|')
                 st.session_state.courtInfo = CourtInfo(spamReader)
         if 'winnerTeam' not in st.session_state:
-            st.session_state.winnerTeam = 'None'
+            st.session_state.winnerTeam = None
         st.header(st.session_state.courtInfo.name)
         
         
     def Window1(self):
-        for player in st.session_state.courtInfo.players:
-            st.image(Image.open(path.join(path.abspath('Resources'), 'person.jpg')), caption=player.name, width=100)
-        st.button("Start", type="primary", on_click=GoWindows2)
-        # start = CTK.CTkButton(self.__window1, text='Start', command=self.GoWindows2)
-        # start.grid(row=2, column=0, sticky="nsew", padx=5, pady=5)
+        gameIdx = 1
+        col1, col2 = st.columns(2, vertical_alignment="center")
+        with col1:
+            for player in st.session_state.courtInfo.teams['Team 1'][str(gameIdx)]:
+                st.image(Image.open(path.join(path.abspath('Resources'), 'person.jpg')), caption=player.name, width=50)
+        with col2:
+            for player in st.session_state.courtInfo.teams['Team 2'][str(gameIdx)]:
+                st.image(Image.open(path.join(path.abspath('Resources'), 'person.jpg')), caption=player.name, width=50)
+        st.button("Start", type="primary", on_click=secondPage)
         
     def Window2(self):
         nCombination = len(st.session_state.courtInfo.teams['Team 1'])
         gameIdx = (st.session_state.courtInfo.gameIdx % nCombination) if (st.session_state.courtInfo.gameIdx % nCombination) != 0 else nCombination
         prevGameIdx = gameIdx if (st.session_state.courtInfo.gameIdx == 1) else  gameIdx - 1 if gameIdx - 1 > 0 else nCombination
 
-        st.subheader(f'Game {st.session_state.courtInfo.gameIdx}')
+        st.subheader(f'Game #{st.session_state.courtInfo.gameIdx}')
         col1, col2 = st.columns(2, vertical_alignment="center")
         with col1:
             st.markdown(f'Team 1')
             for player in st.session_state.courtInfo.teams['Team 1'][str(gameIdx)]:
-                st.image(Image.open('./Resources/person.jpg'), caption=player.name, width=100)
+                st.image(Image.open(path.join(path.abspath('Resources'), 'person.jpg')), caption=player.name, width=50)
         with col2:
             st.markdown(f'Team 2')
             for player in st.session_state.courtInfo.teams['Team 2'][str(gameIdx)]:
-                st.image(Image.open('./Resources/person.jpg'), caption=player.name, width=100)
+                st.image(Image.open(path.join(path.abspath('Resources'), 'person.jpg')), caption=player.name, width=50)
+        
         if len(st.session_state.courtInfo.winnerTeam) >= st.session_state.courtInfo.gameIdx:
-            if st.session_state.courtInfo.winnerTeam[st.session_state.courtInfo.gameIdx - 1] == 'Team 1': 
-                st.session_state.winnerTeam = st.radio(f"Who won the Game {st.session_state.courtInfo.gameIdx}:",["Team 1", "Team 2"], horizontal=True, index=0)
-                try:
-                    st.session_state.courtInfo.winnerTeam[st.session_state.courtInfo.gameIdx-1] = "Team 1"
-                except:
-                    st.session_state.courtInfo.winnerTeam.append("Team 1")
-            elif st.session_state.courtInfo.winnerTeam[st.session_state.courtInfo.gameIdx - 1] == 'Team 2': 
-                st.session_state.winnerTeam = st.radio(f"Who won the Game {st.session_state.courtInfo.gameIdx}:",["Team 1", "Team 2"], horizontal=True, index=1)
-                try:
-                    st.session_state.courtInfo.winnerTeam[st.session_state.courtInfo.gameIdx-1] = "Team 2"
-                except:
-                    st.session_state.courtInfo.winnerTeam.append("Team 2")
+            st.session_state.winnerTeam = st.session_state.courtInfo.winnerTeam[st.session_state.courtInfo.gameIdx - 1]
+        if st.session_state.winnerTeam == 'Team 1': 
+            st.session_state.winnerTeam = st.radio(f"Who won the Game #{st.session_state.courtInfo.gameIdx}:",["Team 1", "Team 2"], horizontal=True, index=0)
+        elif st.session_state.winnerTeam == 'Team 2': 
+            st.session_state.winnerTeam = st.radio(f"Who won the Game #{st.session_state.courtInfo.gameIdx}:",["Team 1", "Team 2"], horizontal=True, index=1)
         else:
-            st.session_state.winnerTeam = st.radio(f"Who won the Game {st.session_state.courtInfo.gameIdx}:",["Team 1", "Team 2"], horizontal=True, index=None)
+            st.session_state.winnerTeam = st.radio(f"Who won the Game #{st.session_state.courtInfo.gameIdx}:",["Team 1", "Team 2"], horizontal=True, index=None)
+        
         col1, col2 = st.columns(2, vertical_alignment="center")
         with col1:
-            st.button("Prev Game", on_click=self.PrevGame, disabled= st.session_state.winnerTeam == 'None')
+            st.button("Prev Game", on_click=self.PrevGame, disabled= st.session_state.winnerTeam == None)
         with col2:
-            st.button("Next Game", on_click=self.NextGame, disabled= st.session_state.winnerTeam == 'None')
-        
+            st.button("Next Game", on_click=self.NextGame, disabled= st.session_state.winnerTeam == None)
+        st.button("Finish Loboton", on_click=self.FinishLoboton, disabled= st.session_state.winnerTeam == None)
+
     def NextGame(self):
+        try:
+            st.session_state.courtInfo.winnerTeam[st.session_state.courtInfo.gameIdx-1] = st.session_state.winnerTeam
+        except:
+            st.session_state.courtInfo.winnerTeam.append(st.session_state.winnerTeam)
         st.session_state.courtInfo.gameIdx += 1
+        st.session_state.winnerTeam = None
 
     def PrevGame(self):
+        try:
+            st.session_state.courtInfo.winnerTeam[st.session_state.courtInfo.gameIdx-1] = st.session_state.winnerTeam
+        except:
+            st.session_state.courtInfo.winnerTeam.append(st.session_state.winnerTeam)
         if st.session_state.courtInfo.gameIdx == 1: return
         st.session_state.courtInfo.gameIdx -= 1
+        st.session_state.winnerTeam = None
 
-    # def FinishLoboton(self):
-    #     self.__courtInfo.Finish()
-    #     self.__window2.destroy()
-    #     self.Window3()
-        
-    def EnableWin2Buttons(self, winnerTeam):
+    def FinishLoboton(self):
         try:
-            st.session_state.courtInfo.winnerTeam[st.session_state.courtInfo.gameIdx-1] = winnerTeam
+            st.session_state.courtInfo.winnerTeam[st.session_state.courtInfo.gameIdx-1] = st.session_state.winnerTeam
         except:
-            st.session_state.courtInfo.winnerTeam.append(winnerTeam)
+            st.session_state.courtInfo.winnerTeam.append(st.session_state.winnerTeam)
+        st.session_state.courtInfo.Finish()
+        thirdPage()
 
-    # def Window3(self):
+    def Window3(self):
+        gameIdx = 1
+        col1, col2 = st.columns(2, vertical_alignment="center")
+        with col1:
+            for player in st.session_state.courtInfo.teams['Team 1'][str(gameIdx)]:
+                st.image(Image.open(path.join(path.abspath('Resources'), 'person.jpg')), caption=f'{player.name} : {player.wonGames}/{st.session_state.courtInfo.gameIdx}', width=50)
+        with col2:
+            for player in st.session_state.courtInfo.teams['Team 2'][str(gameIdx)]:
+                st.image(Image.open(path.join(path.abspath('Resources'), 'person.jpg')), caption=f'{player.name} : {player.wonGames}/{st.session_state.courtInfo.gameIdx}', width=50)
+
     #     self.__window1 = CTK.CTkFrame(self)
     #     self.__window1.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
     #     self.__window1.grid_rowconfigure(1, weight=1)
@@ -116,4 +133,7 @@ if __name__ == '__main__':
         lobotonSheet.Window1()
     elif st.session_state.page == 1:
         lobotonSheet.Window2()
+    elif st.session_state.page == 2:
+        lobotonSheet.Window3()
+        
     
