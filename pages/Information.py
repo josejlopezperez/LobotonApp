a1d0c4b442afc1c441ad35eab2f2faa5da46273a
Pepechuy <jose.lopez-perez@volvo.com>
2025-07-29 08:56:54 -0400

    [REFAC] Remove download button for court player files in Information page

--- pages/Information.py
+++ pages/Information.py
@@ -5,35 +5,14 @@ import pandas as pd
 with open(path.join(path.abspath('Resources'), 'court_6_players.csv'), newline='') as csvFile:
     df = pd.read_csv(csvFile, index_col=0)
     st.write("Example of a court file with 6 players:")
-    st.dataframe(df,)
-    csv = df.to_csv(index=False)
-    st.download_button(
-        label="Download court file",
-        data=csv,
-        file_name='court_6_players.csv',
-        mime='text/csv'
-    )
+    st.dataframe(df)
 
 with open(path.join(path.abspath('Resources'), 'court_7_players.csv'), newline='') as csvFile:
     df = pd.read_csv(csvFile, index_col=0)
     st.write("Example of a court file with 7 players:")
     st.dataframe(df)
-    csv = df.to_csv(index=False)
-    st.download_button(
-        label="Download court file",
-        data=csv,
-        file_name='court_7_players.csv',
-        mime='text/csv'
-    )
     
 with open(path.join(path.abspath('Resources'), 'court_8_players.csv'), newline='') as csvFile:
     df = pd.read_csv(csvFile, index_col=0)
     st.write("Example of a court file with 8 players:")
     st.dataframe(df)
-    csv = df.to_csv(index=False)
-    st.download_button(
-        label="Download court file",
-        data=csv,
-        file_name='court_8_players.csv',
-        mime='text/csv'
-    )
