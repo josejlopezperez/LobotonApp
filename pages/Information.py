565be6c4add2019c4edf20ce96e9ec6123ac39dc
Pepechuy <jose.lopez-perez@volvo.com>
2025-07-30 08:57:47 -0400

    [FEAT] Replace dataframe display with data editor for court player files in Information page

--- pages/Information.py
+++ pages/Information.py
@@ -5,14 +5,14 @@ import pandas as pd
 with open(path.join(path.abspath('Resources'), 'court_6_players.csv'), newline='') as csvFile:
     df = pd.read_csv(csvFile, index_col=0)
     st.write("Example of a court file with 6 players:")
-    st.dataframe(df)
+    st.data_editor(df)
 
 with open(path.join(path.abspath('Resources'), 'court_7_players.csv'), newline='') as csvFile:
     df = pd.read_csv(csvFile, index_col=0)
     st.write("Example of a court file with 7 players:")
-    st.dataframe(df)
+    st.data_editor(df)
     
 with open(path.join(path.abspath('Resources'), 'court_8_players.csv'), newline='') as csvFile:
     df = pd.read_csv(csvFile, index_col=0)
     st.write("Example of a court file with 8 players:")
-    st.dataframe(df)
+    st.data_editor(df)
